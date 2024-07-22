76e9ca0338303f469174147e06ed4d3b577002b6
pranali_dalvi-8 <pranalidalvi8123@gmail.com>
2024-07-22 14:39:04 +0530

    prop drilling counter is added

---
+++ react-context/src/App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import Navbar from './Navbar';
+import { useState } from 'react';
+
+function App() {
+  const [count , setCount]=useState(0);
+  return (
+    <>
+
+    
+    <div className="App">
+    <Navbar count={count} setCount={setCount} />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       
+<button onClick={()=>setCount(count+1)}>
+  count is {count}
+</button>
+
+     
+      </header>
+    </div>
+    </>
+  );
+}
+
+export default App;
